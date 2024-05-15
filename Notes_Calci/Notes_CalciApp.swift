7b854bf36c6de025eaf989375ea21a5953ef8aba
jahnavi-bounteous <153906959+jahnavi-bounteous@users.noreply.github.com>
2024-05-15 11:21:24 +0530

    Initial Commit

---
+++ Notes_Calci/Notes_CalciApp.swift
@@ -0,0 +1,17 @@
+//
+//  Notes_CalciApp.swift
+//  Notes_Calci
+//
+//  Created by Jahnavi Vemuri on 15/05/24.
+//
+
+import SwiftUI
+
+@main
+struct Notes_CalciApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
